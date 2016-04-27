a50a6f88bc8cbe88e4152443189fab3236878bc9
Ivo Maixner <ivo.maixner@gmail.com>
2016-04-27 12:48:13 +0200

    Externalize db configuration into a config file.

--- src/clojure_jdbc_sample/db_setup.clj
+++ src/clojure_jdbc_sample/db_setup.clj
@@ -2,20 +2,13 @@
   (:require [hikari-cp.core :as ds]))
 
 (def datasource-config
-  {:adapter "h2"
-   :url "jdbc:h2:file:./target/db/sample;AUTO_SERVER=TRUE;DB_CLOSE_ON_EXIT=FALSE;MVCC=TRUE;TRACE_LEVEL_FILE=4"
-   :username "sa"
-   :password ""
-   :maximum-pool-size 2
-   :minimum-idle 2
-  })
+  (read-string (slurp "resources/db/db-config.edn")))
 
 (def datasource
   (ds/make-datasource datasource-config))
 
 (def db-spec
-  {:datasource datasource
-  })
+  {:datasource datasource})
 
 (defn close-datasource []
   (ds/close-datasource datasource))
