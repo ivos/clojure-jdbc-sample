42a7695ece37bfe8e8edde5921b6556d29f12adb
Ivo Maixner <ivo.maixner@gmail.com>
2016-04-27 13:16:03 +0200

    Add db transaction.

--- src/clojure_jdbc_sample/core.clj
+++ src/clojure_jdbc_sample/core.clj
@@ -3,6 +3,7 @@
   (:require [clojure-jdbc-sample.db-setup :refer [db-spec close-datasource]]
             [clojure-jdbc-sample.repo :as repo]
             [clojure-jdbc-sample.migration :as migration]
+            [clojure.java.jdbc :as db]
             [hugsql.core :refer [def-db-fns]]
   ))
 
@@ -12,18 +13,23 @@
 (def-db-fns "db/sql/customer.sql"
   {:connection db-spec})
 
+(defn setup-data []
+  (db/with-db-transaction [tc db-spec]
+    (let [alpha1 (repo/create! tc :customer {:name "Alpha"})
+          alpha2 (repo/update! tc :customer (assoc alpha1 :email "info@alpha.com"))
+          alpha3 (repo/update! tc :customer (assoc alpha2 :phone "+2-000-888-666"))
+          beta (repo/create! tc :customer {:name "Beta" :email "info@beta.com" :phone "+1-234-555-678"})
+;          _ (throw (RuntimeException. "Bla"))
+          all (list-all-customers tc)
+          alpha4 (get-customer tc alpha1)
+          alpha5 (repo/delete! tc :customer alpha3)
+          after-delete (list-all-customers tc)]
+      (println "All:" all)
+      (println "Alpha4:" alpha4)
+      (println "After delete:" after-delete)
+  )))
+
 (defn -main [& args]
-  (let [alpha1 (repo/create! db-spec :customer {:name "Alpha"})
-        alpha2 (repo/update! db-spec :customer (assoc alpha1 :email "info@alpha.com"))
-        alpha3 (repo/update! db-spec :customer (assoc alpha2 :phone "+2-000-888-666"))
-        beta (repo/create! db-spec :customer {:name "Beta" :email "info@beta.com" :phone "+1-234-555-678"})
-        all (list-all-customers db-spec)
-        alpha4 (get-customer db-spec alpha1)
-        alpha5 (repo/delete! db-spec :customer alpha3)
-        after-delete (list-all-customers db-spec)]
-    (println "All:" all)
-    (println "Alpha4:" alpha4)
-    (println "After delete:" after-delete)
-    (close-datasource)
-  )
+  (setup-data)
+  (close-datasource)
 )
